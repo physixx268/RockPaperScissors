7937ae28c9f23f4691e5a4600509aead3f1ba481
physixx268 <53198392+physixx268@users.noreply.github.com>
2022-12-20 15:32:49 +0100

    added first inputs

--- script.js
+++ script.js
@@ -18,21 +18,26 @@ function getCpuChoice() {
 function play_round(player, cpu) {
     //tie
     if (player == cpu) {
+        console.log("It's a tie!");
         return "It's a tie!";
     }
     //rock
     if (player == "rock") {
         if (cpu == "paper") {
+            console.log("You lose! Paper beats Rock");
             return "You lose! Paper beats Rock";
         } else {
+            console.log("You won! Rock beats Scissors");
             return "You won! Rock beats Scissors"
         }
     }
     //paper
     if (player == "paper") {
         if (cpu == "rock") {
+            console.log("You won!  Paper beats Rock");
             return "You won! Paper beats Rock";
         } else {
+            console.log("You lose! Scissors beats Paper");
             return "You lose! Scissors beats Paper";
         }
 
@@ -42,8 +47,10 @@ function play_round(player, cpu) {
     if (player
         == "scissors") {
         if (cpu == "rock") {
+            console.log("You lose! Rock beats Scissors");
             return "You lose! Rock beats Scissors";
         } else {
+            console.log("You won! Scissors beats Paper");
             return "You won! Scissors beats Paper";
         }
     }
@@ -66,13 +73,28 @@ function game(rounds) {
         const result = play_round(playerSelection, getCpuChoice);
         console.log(result);
         //play_round(playerSelection, getCpuChoice())
-        //display_result(play_round(playerSelection, getCpuChoice));
         display_result(result);
     }
 }
 
 // get user input for rounds to play and convert to int
-let rounds = prompt("How many rounds you wanna play?");
-rounds = parseInt(rounds);
+//let rounds = prompt("How many rounds you wanna play?");
+//rounds = parseInt(rounds);
 
-game(rounds);
+const btnRock = document.getElementById("btnRock");
+const btnPaper = document.getElementById("btnPaper");
+const btnScissors = document.getElementById("btnScissors");
+
+rounds = 1;
+
+btnRock.addEventListener('click', (e) => {
+    play_round("rock", getCpuChoice());
+} );
+btnPaper.addEventListener("click", (e) => {
+    play_round("paper", getCpuChoice());
+} );
+btnScissors.addEventListener("click", (e) => {
+    play_round("scissors", getCpuChoice());
+} );
+
+//game(rounds);
